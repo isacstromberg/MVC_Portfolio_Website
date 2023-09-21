e4881b1123ff7ac71ac0f9e9c718a079bb89b6db
Isac Strömberg <isacstromberg@hotmail.com>
2023-09-21 15:19:40 +0200

    Add project files.

---
+++ MVC_Portfolio_Website/Controllers/EstatesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Portfolio_Website.Controllers
+{
+    public class EstatesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
